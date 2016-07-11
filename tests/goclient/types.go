d4b84b605961413e2124962695433a1aa8906675
Taka Wang <wang.chia.ming@gmail.com>
2016-07-11 14:40:54 +0800

    update types

--- tests/goclient/types.go
+++ tests/goclient/types.go
@@ -94,7 +94,7 @@ type DMbtcpRes struct {
 	Tid string `json:"tid"`
 	// Status response status string
 	Status string `json:"status"`
-	// Data for read function code only.
+	// Data for 'read function code' only.
 	Data []uint16 `json:"data,omitempty"`
 }
 
@@ -104,11 +104,11 @@ type DMbtcpReadReq struct {
 	Tid string `json:"tid"`
 	// Cmd modbusd command type: https://github.com/taka-wang/modbusd#command-mapping-table
 	Cmd int `json:"cmd"`
-	// IP modbus tcp slave ip address or hostname
+	// IP ip address or hostname of the modbus tcp slave
 	IP string `json:"ip"`
-	// Port port number
+	// Port port number of the modbus tcp slave
 	Port string `json:"port"`
-	// Slave modbus device id
+	// Slave device id of the modbus tcp slave
 	Slave uint8 `json:"slave"`
 	// Addr start address for read
 	Addr uint16 `json:"addr"`
@@ -122,11 +122,11 @@ type DMbtcpWriteReq struct {
 	Tid string `json:"tid"`
 	// Cmd modbusd command type: https://github.com/taka-wang/modbusd#command-mapping-table
 	Cmd int `json:"cmd"`
-	// IP modbus tcp slave ip address or hostname
+	// IP ip address or hostname of the modbus tcp slave
 	IP string `json:"ip"`
-	// Port port number
+	// Port port number of the modbus tcp slave
 	Port string `json:"port"`
-	// Slave modbus device id
+	// Slave device id of the modbus tcp slave
 	Slave uint8 `json:"slave"`
 	// Addr start address for write
 	Addr uint16 `json:"addr"`
@@ -150,10 +150,10 @@ type DMbtcpTimeout struct {
 
 // ======================= services to psmb structures - upstream =======================
 
-// MbtcpOnceReadReq read coil/register request (1.1).
+// MbtcpReadReq read coil/register request (1.1).
 // Scale range field example:
 // Range: &ScaleRange{1,2,3,4},
-type MbtcpOnceReadReq struct {
+type MbtcpReadReq struct {
 	Tid   int64        `json:"tid"`
 	From  string       `json:"from,omitempty"`
 	FC    int          `json:"fc"`
@@ -167,10 +167,10 @@ type MbtcpOnceReadReq struct {
 	Range *ScaleRange  `json:"range,omitempty"` // point to struct can be omitted in json encode
 }
 
-// MbtcpOnceReadRes read coil/register response (1.1).
+// MbtcpReadRes read coil/register response (1.1).
 // `Data interface` supports:
 // []uint16, []int16, []uint32, []int32, []float32, string
-type MbtcpOnceReadRes struct {
+type MbtcpReadRes struct {
 	Tid    int64        `json:"tid"`
 	Status string       `json:"status"`
 	Type   RegValueType `json:"type,omitempty"`
