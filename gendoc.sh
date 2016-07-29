bb8de990c4a69800a1459bcbb8fab2b477ec29dd
Taka Wang <wang.chia.ming@gmail.com>
2016-07-30 00:08:33 +0800

    update gendoc script

--- gendoc.sh
+++ gendoc.sh
@@ -18,7 +18,7 @@ mkdir code_docs
 cd code_docs
 
 # Get the current gh-pages branch
-git clone -b gh-pages https://git@$GH_REPO_REF
+git clone -b gh-pages https://${GH_REPO_TOKEN}@$GH_REPO_REF
 cd $GH_REPO_NAME
 
 ##### Configure git.
