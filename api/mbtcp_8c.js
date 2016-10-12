a6280d6d6ee80ed63803afe836491e66e9bfd02e
Taka Wang <wang.chia.ming@gmail.com>
2016-10-12 16:48:14 +0800

    Deploy docs to GitHub Pages Travis build: 451
    Commit: da500b0838f77827da2b8b6b6a6d4a88f59074f2

--- api/mbtcp_8c.js
+++ api/mbtcp_8c.js
@@ -1,15 +1,7 @@
 var mbtcp_8c =
 [
-    [ "mbtcp_cmd_hanlder", "mbtcp_8c.html#a02e79cd2e10e04ca269f511ba9085b24", null ],
+    [ "mbtcp_cmd_hanlder", "mbtcp_8c.html#a9ebfe4b9c143c562a97772ef570552a9", null ],
     [ "mbtcp_do_connect", "mbtcp_8c.html#a819f6eb596caa2acb6344aecd5daf51a", null ],
-    [ "mbtcp_fc15_request", "mbtcp_8c.html#a40780292d66d73db87b4d63a3467bf87", null ],
-    [ "mbtcp_fc16_request", "mbtcp_8c.html#a7a877d48611ccac756984d29f9f59d37", null ],
-    [ "mbtcp_fc1_request", "mbtcp_8c.html#acec7eb8cd26d64c73ca68531abb35abe", null ],
-    [ "mbtcp_fc2_request", "mbtcp_8c.html#ac30955718174af366207e23931eed63b", null ],
-    [ "mbtcp_fc3_request", "mbtcp_8c.html#a203227647298a0fe6d401088009f0a52", null ],
-    [ "mbtcp_fc4_request", "mbtcp_8c.html#a70b5b676d39d2b6ced03699a46d0e671", null ],
-    [ "mbtcp_fc5_request", "mbtcp_8c.html#acf0a8a74de44e63938833dfd98d48f0f", null ],
-    [ "mbtcp_fc6_request", "mbtcp_8c.html#a3742b55d75924653baa1ce94b5ee5afe", null ],
     [ "mbtcp_get_connection_status", "mbtcp_8c.html#a10fdba0f8df3a84f6a1edd2ce82fb746", null ],
     [ "mbtcp_get_handle", "mbtcp_8c.html#a457458d6f5fef93f9bac1bf9365b8a61", null ],
     [ "mbtcp_get_response_timeout", "mbtcp_8c.html#a3e54b1362b644a8f7da785e9a7b3f561", null ],
--- api/search/typedefs_1.js
+++ api/search/typedefs_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['mbtcp_5ffc',['mbtcp_fc',['../mb_8h.html#a4a73aba240ae86cd18cebbc0bcb4a01b',1,'mb.h']]]
+  ['mbtcp_5ffc',['mbtcp_fc',['../mb_8h.html#a6883b763e739f8250ac7f612f6e6e2ff',1,'mb.h']]]
 ];
